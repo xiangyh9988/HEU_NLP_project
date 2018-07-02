53fedd942a15244f839ff02b702b16924f62851b
LittleFlyingSheep <38974045+LittleFlyingSheep@users.noreply.github.com>
2018-07-02 22:08:41 +0800

    Revert "framework"

--- src/main/java/com/example/mysql/service/SentimentService.java
+++
@@ -1,9 +0,0 @@
-package com.example.mysql.service;
-
-import com.alibaba.fastjson.JSONObject;
-
-import java.io.IOException;
-
-public interface SentimentService {
-    public JSONObject analyze(JSONObject jsonObject) throws IOException;
-}
